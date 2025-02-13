b93116a1ce494b7be892d2978c718daf6f802ceb
Rifky <s22010240@ousl.lk>
2025-02-13 15:52:21 +0530

    working-with efforct-deps

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import QUESTIONS from "../questions.js";
-import quizCompleteImg from '../assets/quiz-complete.png';
+import quizCompleteImg from "../assets/quiz-complete.png";
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
@@ -8,32 +9,42 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswer.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswer((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>QuizCompleted!</h2>
+      </div>
+    );
   }
 
-  if (quizIsComplete){
-    return(
-        <div id="summary">
-            <img src={quizCompleteImg} alt="Trophy icon"/>
-            <h2>QuizCompleted!</h2>
-        </div>
-    )
-  }
-
-// Create a copy of the answers array for the current question
-const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-// Shuffle the answers array randomly
-shuffledAnswers.sort(() => Math.random() - 0.5);
+  // Create a copy of the answers array for the current question
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  // Shuffle the answers array randomly
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
+
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
