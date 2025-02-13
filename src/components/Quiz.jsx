92d7dc0f0f57d5d3cdbde0faeffe0a4935142af7
Rifky <s22010240@ousl.lk>
2025-02-13 14:12:40 +0530

    first-cmp-some-state

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  return <p>Currently active quetion</p>;
+}
