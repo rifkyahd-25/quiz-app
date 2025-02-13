a4f3f92f271f6943266a26924ad6012cbb69167e
Rifky <s22010240@ousl.lk>
2025-02-13 20:04:28 +0530

    finished

---
+++ src/components/Question.jsx
@@ -0,0 +1,50 @@
+import QUESTIONS from "../questions.js";
+import Answers from "./Answers.jsx";
+import QuestionTimer from "./QuestionTimer.jsx";
+import { useState } from "react";
+
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      inCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
+
+  let answerState = "";
+
+  if (answer.selectedAnswer && answer.isCorrect !== null) {
+    answerState = answer.isCorrect ? "correct" : "wrong";
+  } else if (answer.selectedAnswer) {
+    answerState = "answered";
+  }
+
+  return (
+    <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{QUESTIONS[index].text}</h2>
+      <Answers
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
+        answerState={answerState}
+        onSelect={handleSelectAnswer}
+      />
+    </div>
+  );
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
@@ -9,9 +10,7 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswer.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    selectedAnswer
-  ) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswer((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
@@ -32,27 +31,41 @@ export default function Quiz() {
     );
   }
 
-  // Create a copy of the answers array for the current question
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  // Shuffle the answers array randomly
-  shuffledAnswers.sort(() => Math.random() - 0.5);
-
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
 
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Question
+      key={activeQuestionIndex}
+      index={activeQuestionIndex}
+      onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
+      
     </div>
-  );
-}
+  )
+};
+
+//   // Create a copy of the answers array for the current question
+//   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+//   // Shuffle the answers array randomly
+//   shuffledAnswers.sort(() => Math.random() - 0.5);
+
+//   return (
+//     <div id="quiz">
+//       <div id="question">
+//         <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
+
+//         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+//         <ul id="answers">
+//           {shuffledAnswers.map((answer) => (
+//             <li key={answer} className="answer">
+//               <button onClick={() => handleSelectAnswer(answer)}>
+//                 {answer}
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
