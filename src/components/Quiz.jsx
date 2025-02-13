c21ba95b1e10e7ab0188e44c89bedc9e0a7a3e25
Rifky <s22010240@ousl.lk>
2025-02-13 15:29:53 +0530

    suffling-answer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from '../assets/quiz-complete.png';
+
 export default function Quiz() {
   const [userAnswer, setUserAnswer] = useState([]);
 
   const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswer((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
   }
+
+  if (quizIsComplete){
+    return(
+        <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy icon"/>
+            <h2>QuizCompleted!</h2>
+        </div>
+    )
+  }
+
+// Create a copy of the answers array for the current question
+const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+// Shuffle the answers array randomly
+shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
